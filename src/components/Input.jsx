d4557aa870ec95bb2a973a19d69091fd11ae4e41
magagr17 <markjohnaguirre@gmail.com>
2023-01-31 12:12:06 +0800

    created/separated ColorPicker  as new component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,65 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import ColorPicker from './ColorPicker';
 
 const Input = (props) => {
-    const [input, setInput] = useState({title:"", content:""})
+  const [input, setInput] = useState({ title: '', content: '', noteColor:"" });
 
-    const [colorChange, setColorChange]= useState(false);
-    const colors = ['#fffd8d', '#aed143', '#f49f3f', '#d35595', '#51bcb3'];
+  
 
- const handleChange = (e) => {
-    const {value, name} = e.target;
-    
-    setInput((prevValue)=> { 
-        return{
-        ...prevValue, [name]:value
-        }
- })}
+  const handleChange = (e) => {
+    const { value, name } = e.target;
 
-const onSubmit = () => {
-    props.onAdd(input)
-    setInput({title:"", content:""})
-}
+    setInput((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
 
-const [bgColor, setBgColor] = useState('#fffd8d');
+  const onSubmit = () => {
+    props.onAdd(input);
+    setInput({ title: '', content: '', noteColor:"" });
+  };
 
 
-const selectColor = item => {
-    setBgColor(colors[item])
-   
-}
+  const [bgColor, setBgColor] = useState('#fffd8d');
+
+  const changeColor = (newColor) => {
+    setBgColor(newColor);
+
+  };
  
 
-
-    return ( 
-        <div>
-            <div className='font-mont flex justify-center py-3'>
-                <div style={{backgroundColor: bgColor }} className='w-[500px] h-[200px] px-4 py-2 shadow-xl relative flex flex-col rounded-xl'>
-                    <input onChange={handleChange} name="title" value={input.title} type="text" placeholder="Title" className='w-full h-[50px] bg-transparent font-bold outline-none ' />
-                    <textarea onChange={handleChange} name="content" value={input.content} type="text" placeholder="Take a note.." className='w-full h-[100px] bg-transparent outline-none whitespace-pre-wrap' />
-                    <button onClick={()=>setColorChange(!colorChange)} className='text-sm text-black/80 font-bold rounded-lg w-[120px] border border-black/40 absolute right-3 top-3 '>Change Color</button>
-                    <ul style={{display: colorChange && 'flex'}} className='hidden bg-black/30 w-[300px] justify-center gap-2 p-2 rounded-lg absolute top-10 right-3'>
-                       
-                        {colors.map((item,index) => {
-                          return  <div>
-                             <li onClick={()=> selectColor(index)} key={index} className='w-[50px] h-[50px] border-white border border-4 rounded-lg cursor-pointer hover:scale-105' style={{backgroundColor: item }}></li> 
-                             </div>
-                        })} 
-                    </ul>
-                    <button onClick={onSubmit} className='rounded-full text-sm font-bold text-white bg-yellow-500 w-[70px] h-[50px] absolute right-5 -bottom-3 cursor-pointer hover:scale-105'>Add</button>
-                </div>
-
-
-
-            </div>
-
-
-
+  return (
+    <div>
+      <div className="font-mont flex justify-center py-3">
+        <div
+          style={{ backgroundColor: bgColor }}
+          className="w-[500px] h-[200px] px-4 py-2 shadow-xl relative flex flex-col rounded-xl "
+        >
+          <input
+            onChange={handleChange}
+            name="title"
+            value={input.title}
+            type="text"
+            placeholder="Title"
+            className="w-full h-[50px] bg-transparent font-bold outline-none placeholder-black/50"
+          />
+          <textarea
+            onChange={handleChange}
+            name="content"
+            value={input.content}
+            type="text"
+            placeholder="Take a note.."
+            className="w-full h-[100px] bg-transparent outline-none whitespace-pre-wrap placeholder-black/50"
+          />
+          <ColorPicker selectColor = {changeColor} />
+          <button
+            onClick={onSubmit}
+            className="rounded-full text-sm font-bold text-white bg-yellow-500 w-[70px] h-[50px] absolute right-5 -bottom-3 cursor-pointer hover:scale-105"
+          >
+            Add
+          </button>
         </div>
-
-
-
-
-     );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Input;
