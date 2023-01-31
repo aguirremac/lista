90d8320df158519270d424767ac52f08a145e11d
magagr17 <markjohnaguirre@gmail.com>
2023-01-31 15:37:24 +0800

    added addnote feature where input note shows only when clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import {BsPlusSquareDotted} from 'react-icons/bs';
 
 
-const Header = () => {
+
+const Header = (props) => {
+    
+
+
 return (
- <header className="bg-blue-400 text-white mx-auto p-4 font-semibold font-mont flex">
+ <header className="bg-[#51bcb3] text-white mx-auto p-4 font-semibold font-mont flex justify-between px-10">
+    <div className="flex items-center ">
     <img src="https://img.icons8.com/fluency/512/sticky-notes.png" className="h-[50px]"></img>
     <h1 className="tracking-widest  text-5xl pl-2 ">Lista</h1>
+    </div>
+
+    <div onClick={props.handleClick} className="flex items-center  gap-5 cursor-pointer hover:scale-105">
+    <BsPlusSquareDotted size={40}  className=" "/>
+        <p className="text-2xl cursor-pointer ">Add Note</p>
+    </div>
     </header>
 )
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import ColorPicker from './ColorPicker';
+import Header from './Header';
+import { IoMdClose } from 'react-icons/io';
+
 
 const Input = (props) => {
   
   const [input, setInput] = useState({ title: '', content: '', noteColor:'#fffd8d'});
   const [bgColor, setBgColor] = useState('#fffd8d');
+  
   
 
   
@@ -45,18 +49,20 @@ const Input = (props) => {
 
   return (
     <div>
-      <div className="font-mont flex justify-center py-3">
+        
+      <div className="font-mont justify-center py-3 flex absolute top-[100px] right-10 z-10 drop-shadow-2xl">
         <div
           style={{ backgroundColor: bgColor }}
-          className="w-[500px] h-[200px] px-4 py-2 shadow-xl relative flex flex-col rounded-xl "
+          className="w-[500px] h-[400px] px-4 py-2  relative flex flex-col rounded-xl "
         >
+            <IoMdClose size={25} onClick={props.handleClick} className="absolute right-3 cursor-pointer hover:text-red-600"/>
           <input
             onChange={handleChange}
             name="title"
             value={input.title}
             type="text"
             placeholder="Title"
-            className="w-full h-[50px] bg-transparent font-bold outline-none placeholder-black/50"
+            className="w-full h-[50px] bg-transparent font-bold outline-none placeholder-black/50 text-xl"
           />
           <textarea
             onChange={handleChange}
@@ -64,12 +70,12 @@ const Input = (props) => {
             value={input.content}
             type="text"
             placeholder="Take a note.."
-            className="w-full h-[100px] bg-transparent outline-none whitespace-pre-wrap placeholder-black/50"
+            className="w-full h-[300px] bg-transparent outline-none whitespace-pre-wrap placeholder-black/50"
           />
           <ColorPicker onClick={handleChange} selectColor = {changeColor} name='noteColor' />
           <button
             onClick={onSubmit}
-            className="rounded-full text-sm font-bold text-white bg-yellow-500 w-[70px] h-[50px] absolute right-5 -bottom-3 cursor-pointer hover:scale-105"
+            className="rounded-full text-lg font-bold text-white bg-yellow-500 w-[70px] h-[50px] absolute right-5 -bottom-3 cursor-pointer hover:scale-105"
           >
             Add
           </button>
