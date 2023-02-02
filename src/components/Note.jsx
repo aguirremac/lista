cc143db8cbbd883893b8b56e7b90e636b97984ae
magagr17 <markjohnaguirre@gmail.com>
2023-02-02 10:46:27 +0800

    fixed height issue, changed delete icon

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Input from './Input';
-import {IoMdClose } from 'react-icons/io'
+import {RiDeleteBin6Line } from 'react-icons/ri'
 
 const Note = (props) => {
 
@@ -16,11 +16,11 @@ const Note = (props) => {
      
       {props.notes.map((item, idNumber) => {
         return(
-        <div key={idNumber}  className="font-mont w-[170px] md:w-[300px] h-full overflow-auto  shadow-xl rounded-xl">
-        <div style={{backgroundColor: item.noteColor !== '' ? item.noteColor : '#fffd8d' }} className=" relative p-4 pt-3 ">
-        <IoMdClose  onClick={()=>{ props.onDelete(idNumber)}} size={20} className='absolute top-3 right-3 cursor-pointer hover:text-red-700'/>
-          <h1 className="font-bold pb-2 h-full whitespace-pre-wrap text-xs md:text-xl">{item.title}</h1>
-          <p className='h-full whitespace-pre-wrap text-[10px] md:text-lg'>{item.content}</p>
+        <div key={idNumber}  className="font-mont w-[170px] h-[140px] md:h-[195px] md:w-[300px] h-full overflow-hidden  shadow-xl rounded-xl cursor-pointer hover:scale-105 duration-200">
+        <div style={{backgroundColor: item.noteColor !== '' ? item.noteColor : '#fffd8d' }} className=" relative p-4 pt-3 h-full">
+        <RiDeleteBin6Line  onClick={()=>{ props.onDelete(idNumber)}} size={20} className='absolute top-3 right-3 cursor-pointer hover:text-red-700'/>
+          <h1 className="font-bold pb-2 whitespace-pre-wrap text-xs md:text-xl">{item.title}</h1>
+          <p className='h-full whitespace-pre-wrap text-[10px] md:text-lg '>{item.content}</p>
         </div>
       </div>
         )
