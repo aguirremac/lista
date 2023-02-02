e2e1601523270236709018d229fab37c03ca89ed
magagr17 <markjohnaguirre@gmail.com>
2023-02-02 15:35:51 +0800

    added see more feature where you can zoom in your notes

--- src/App.js
+++ src/App.js
@@ -6,75 +6,85 @@ import { useState } from 'react';
 import Landing from './components/Landing';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
-
+import Edit from './components/Edit';
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [addNote, setAddNote] = useState(false);
+  const [start, setStart] = useState(false);
+  const [seeMore, setSeeMore] = useState([]);
+  const [isSeeMore, setIsSeeMore] = useState(false)
+  
 
-const [notes, setNotes]= useState ([])
-const [addNote, setAddNote] = useState(false)
-const [start, setStart] = useState(false);
+  const handleAdd = (input) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, input];
+    });
+    notify();
+  };
 
-const handleAdd = (input) => {
-  setNotes(prevNotes => {
-    return [
-      ...prevNotes, input
-    ]
-  }); notify()
-}
- 
-console.log(notes);
+
 
   const handleDelete = (idNumber) => {
-    console.log(idNumber)
-    setNotes((prevNotes => {
-       return prevNotes.filter((item,index) =>{
-         return index !== idNumber       })
-     }))
-   
-   }
+    setNotes((prevNotes) => {
+      return prevNotes.filter((item, index) => {
+        return index !== idNumber;
+      });
+    });
+  };
 
+  const showInput = () => {
+    setAddNote(!addNote);
+  };
 
-   const showInput = () => {
-    setAddNote(!addNote)
-   }
-
-
-  const  handleStart = () => {
-    setStart(true)
-  }
+  const handleStart = () => {
+    setStart(true);
+  };
 
   // toastify
- 
+
   const notify = () => {
     toast.success('Note Added!', {
-      position: "bottom-right",
+      position: 'bottom-right',
       autoClose: 2000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
-      theme: "light",
-      });
+      theme: 'light',
+    });
+  };
+
+  const clickSeeMore = (idNumber) => {
+    setSeeMore( notes[idNumber])
+    setIsSeeMore(true)
+    console.log(isSeeMore)
+  };
+
+  const zoomOff = (x) => {
+    setIsSeeMore(x)
   }
 
-
-
-
-
   return (
-    <div >
-      {!start && <Landing onClick ={handleStart}/>}
-      {start && <Header handleClick={showInput} addNoteStatus={addNote}/> }
-      {addNote && <Input onAdd={handleAdd} handleClick={showInput}/>}
-      {start &&  <Note notes={notes} onDelete={handleDelete}/> }
-      <Footer />
-      <ToastContainer /> 
-      </div>
+    <div>
       
+      {!start ? <Landing onClick={handleStart} /> : ( <>
+
+      
+      <Header handleClick={showInput} addNoteStatus={addNote} />
+      {addNote && <Input onAdd={handleAdd} handleClick={showInput} />}
+       <Note
+          notes={notes}
+          onDelete={handleDelete}
+          clickSeeMore={clickSeeMore}
+        />
+      {isSeeMore  && <Edit zoomDetails = {seeMore} zoomOff={zoomOff} />}
+      <Footer />
+      <ToastContainer />
+      </>)}
     
+    </div>
   );
 };
 
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 import Input from './Input';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 
+
 const Note = (props) => {
   // const [notes, setNotes]= useState ([])
 
   return (
     <div>
-      <div className="w-full flex justify-evenly md:justify-start gap-y-3 md:gap-10 pt-3 md:pt-6 flex-wrap flex-start pb-2 md:px-20  z-11 absolute">
+      <div className="w-full flex justify-evenly xl:justify-start gap-y-3 xl:gap-4 pt-3 md:pt-6 flex-wrap flex-start pb-2 md:px-5 xl:px-5 z-11 absolute">
         {props.notes.map((item, idNumber) => {
           return (
             <div
               key={idNumber}
-              className="font-mont w-[170px] h-[183px] md:h-[245px] md:w-[300px]  overflow-hidden  shadow-xl rounded-xl cursor-pointer hover:scale-105 duration-200"
+              className="font-mont w-[170px] h-[180px] md:h-[250px] md:w-[300px]  overflow-hidden  shadow-xl rounded-xl cursor-pointer hover:scale-105 duration-200"
             >
               <div
                 style={{
@@ -27,15 +28,19 @@ const Note = (props) => {
                   }}
                   className="absolute top-3 right-3 text-[15px] md:text-[20px] text-gray-600 cursor-pointer hover:text-red-700 hover:scale-125"
                 />
-                <h1 className="font-bold whitespace-pre-wrap text-xs md:text-xl">
+                <h1 className="font-bold whitespace-pre-wrap text-xs md:text-xl pb-2 ">
                   {item.title}
                 </h1>
-                <p className=" text-[8px] text-gray-600 md:text-[12px] pb-2 md:pb-3">
-                  {item.dateTime}
-                </p>
+                <div className='md:h-[170px] h-[110px] overflow-hidden'>
                 <p className="h-full whitespace-pre-wrap text-[10px] md:text-[15px] font-medium leading-relaxed">
                   {item.content}
                 </p>
+                </div>
+                <p className=" text-[8px] text-gray-600 md:text-[12px] pt-3 md:pt-2 text-right">
+                  {item.dateTime}
+                </p>
+                <p onClick={()=> {props.clickSeeMore(idNumber)}
+                } className='absolute bottom-2 font-semibold text-[10px] md:text-sm text-black hover:text-red-700 hover:font-bold'>See more..</p>
               </div>
             </div>
           );
