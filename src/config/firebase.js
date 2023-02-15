0be0bb4c934a8d166cec8bc36862d0a73111cfa2
magagr17 <markjohnaguirre@gmail.com>
2023-02-16 03:41:40 +0800

    fixed database issue

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgjre_wF-JlH3D7KfNjSrlSVmmiNFACfs",
-  authDomain: "lista-df9fd.firebaseapp.com",
-  projectId: "lista-df9fd",
-  storageBucket: "lista-df9fd.appspot.com",
-  messagingSenderId: "846159749031",
-  appId: "1:846159749031:web:3ae19e21a51e2817afa082",
-  measurementId: "G-13RWDJWKQB"
+  apiKey: 'AIzaSyCse2ApA1lHBnbfvCgfbKkje3UMijST2ok',
+  authDomain: 'lista-2-67122.firebaseapp.com',
+  projectId: 'lista-2-67122',
+  storageBucket: 'lista-2-67122.appspot.com',
+  messagingSenderId: '933901223938',
+  appId: '1:933901223938:web:2c519901a0b073324cbf1b',
 };
 
 const app = initializeApp(firebaseConfig);
