806048a3455226b7f72be8de6e602dff57dc7e2b
magagr17 <markjohnaguirre@gmail.com>
2023-02-15 22:32:30 +0800

    corrected firebase.js setup of db

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAgjre_wF-JlH3D7KfNjSrlSVmmiNFACfs",
@@ -14,5 +15,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const database = getDatabase(app);
+export const auth = getAuth();
+export const db = getFirestore(app);
